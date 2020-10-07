b539b65ac7cc8dbddd55eea4a44d4c0794cc1303
LevDevCorp <L.nats51@yahoo.com>
2020-10-08 00:29:59 +0400

    Updates

---
+++ precache-manifest.c9dff926e8f2ca4ed12d4061cb160ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345104bcf4470f7dcba578acccbd48be",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "ed66737235a2417918de",
+    "url": "/Test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/Test/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed66737235a2417918de",
+    "url": "/Test/static/js/main.82b23673.chunk.js"
+  },
+  {
+    "revision": "d9f4fa90447b19528f52",
+    "url": "/Test/static/js/runtime-main.d9cec00d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
